ae8bd1d6ec1d50723b3f595a8aea096ea58286f4
Artsem <artom.golovenchik@gmail.com>
2017-12-13 11:47:03 +0300

    all labs mmf

---
+++ Lab1/HashCodeComparer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Lab1
+{
+    public class HashCodeComparer<T> : IComparer<T>
+    {
+        public int Compare(T x, T y)
+        {
+            return x.GetHashCode() - y.GetHashCode();
+        }
+    }
+}
